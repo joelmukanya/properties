1d6d81847cdaa54ce1886740db24b0d0a0c0cae3
joelmukanya <joel@lifechoices.co.za>
2022-07-12 23:00:42 +0200

    Updates

--- src/views/AdminComponent.vue
+++ src/views/AdminComponent.vue
@@ -31,10 +31,61 @@
                     <td>{{property.garage}}</td>
                     <td>R{{property.price}}</td>
                     <td>
-                        <span>Edit</span>
+
+                        <i class="bi bi-pencil-square" 
+                        data-bs-toggle="modal" data-bs-target="#exampleModal"></i>
+                        <div class="modal" tabindex="-1" id="exampleModal">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title">Edit</h5>
+                                        <button type="button" class="btn btn-secondary rounded ms-auto" data-bs-dismiss="modal">Close</button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <form class="container">
+                                            <div class="row">
+                                                <input type="text" 
+                                                placeholder="Property Type"/>
+                                            </div>
+                                            <div class="row my-3">
+                                                <input type="text" 
+                                                placeholder="Property Location"/>
+                                            </div>
+                                            <div class="row">
+                                                <input type="text" 
+                                                placeholder="Address"/>
+                                            </div>
+                                            <div class="row my-3">
+                                                <input type="file" 
+                                                placeholder="Picture"/>
+                                            </div>
+                                            <div class="row">
+                                                <input type="number" 
+                                                placeholder="Number of bedrooms"/>
+                                            </div>
+                                            <div class="row my-3">
+                                                <input type="number" 
+                                                placeholder="Number of bathrooms"/>
+                                            </div>
+                                            <div class="row ">
+                                                <input type="number" 
+                                                placeholder="Garage"/>
+                                            </div>
+                                            <div class="row my-3">
+                                                <input type="number" 
+                                                placeholder="Price"/>
+                                            </div>
+                                            <div class="row">
+                                                <button class="btn btn-primary">Submit</button>
+                                            </div>
+                                        </form>     
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </td>
                     <td>
-                        <span>Delete</span>
+                        <span><i class="bi bi-x-circle-fill" @click="deleteItem(index)"></i></span>
                     </td>
                 </tr>
             </tbody>
@@ -52,12 +103,23 @@ export default {
             properties: []
         }
     },
-        async mounted() {
-            // Making use of async 
-            let res = await fetch('http://localhost:3000/properties').
-            catch(e => console.log(e.message));
-            let data = await res.json(); 
-            this.properties = data;
-        }
+    methods: {
+        editItem() {
+
+        },
+        deleteItem(id) {
+            if(id > -1) {
+                this.properties.splice(id, 1);
+            }
+        } 
+
+    },
+    async mounted() {
+        // Making use of async 
+        let res = await fetch('http://localhost:3000/properties').
+        catch(e => console.log(e.message));
+        let data = await res.json(); 
+        this.properties = data;
+    }
 }
 </script>
